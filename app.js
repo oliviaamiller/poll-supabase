57d0964549f6d5e72a49e047bdd24693c5ecf065
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-16 11:05:45 -0800

    added signin and signup functions

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+import { signInUser, signUpUser } from './fetch-utils.js';
+
+const signInFormEl = document.querySelector('#sign-in-form');
+const signUpFormEl = document.querySelector('#sign-up-form');
 
 // let state
 
@@ -6,3 +10,15 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+signInFormEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(signInFormEl);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+
+
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -2,3 +2,16 @@ const SUPABASE_URL = 'https://muqcnskpycwzulcqtssq.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTY3ODI0MiwiZXhwIjoxOTU1MjU0MjQyfQ.YuQgmIqcc4fnbcvXYFvARasJbp47y0LykRMYteRtA04';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+export async function signUpUser(email, password){
+    const response = await client.auth.signUp({ email, password });
+    
+    return response.user;
+}
+
+export async function signInUser(email, password){
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
