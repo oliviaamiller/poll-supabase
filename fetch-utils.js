d31dcc1351c7a95538fc2c508a414e73d54220d6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-16 10:40:22 -0800

    supabase url, cdn, and key added

---
+++ fetch-utils.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://muqcnskpycwzulcqtssq.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTY3ODI0MiwiZXhwIjoxOTU1MjU0MjQyfQ.YuQgmIqcc4fnbcvXYFvARasJbp47y0LykRMYteRtA04';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
